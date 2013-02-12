d155154e693f219ecf0035d0192d97886eedb76e
Olivier Briand <olivier.briand@orange.com>
2013-02-12 11:30:37 +0100

    Simple flip image with xml animations and a viewflipper

--- src/com/obriand/android_flipimage/MainActivity.java
+++ src/com/obriand/android_flipimage/MainActivity.java
@@ -6,13 +6,38 @@ import android.os.Bundle;
 
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
+
+	private ViewFlipper mViewFlipper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mViewFlipper=(ViewFlipper) findViewById(R.id.imageflipper); 
+		
+		mViewFlipper.setInAnimation(this, R.anim.grow_from_middle);
+		mViewFlipper.setOutAnimation(this, R.anim.shrink_to_middle); 
+		
+		mViewFlipper.setOnTouchListener( new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent touchevent) {
+                switch (touchevent.getAction()) {
+	                case MotionEvent.ACTION_DOWN: {
+	                	mViewFlipper.showNext();
+	                    break;
+	                }
+                }
+				return false;
+            }
+		});
+		
 	}
 
 	@Override
